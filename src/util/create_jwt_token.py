415c32e8d1fd9f72524d1731c17d3556ffeb41a4
Ayush Pandey <ayush.pandey@tier.app>
2024-04-21 00:51:29 +0200

    Improvd code for jwt authentication

--- src/util/create_jwt_token.py
+++ src/util/create_jwt_token.py
@@ -7,20 +7,18 @@ from jose import jwt
 from src import settings
 
 
-async def create_access_token(
-    customer_id: uuid, expires_delta: Optional[timedelta] = None
-):
+async def create_access_token(customer_id: uuid, valid_time: Optional[int] = None):
     """
-    :param customer_id: we only use customer_id for the jwt token encodind # TODO: Research why can't I used email,
+    :param customer_id: we only use customer_id for the jwt token encoding # TODO: Research why can't I used email,
     customer_id and language for the jwt token generation
-    :param expires_delta: by default our JWT token will be valid for 15 minutes
+    :param valid_time: by default our JWT token will be valid forever
     :return:
     """
     to_encode = {"customer_id": customer_id}
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+    if valid_time:
+        expire = datetime.utcnow() + timedelta(minutes=valid_time)
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.utcnow()
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(
         to_encode, settings.SECRET_KEY, algorithm="HS256"
---
+++ tests/unit/util/generate_jwt_token.py
@@ -0,0 +1,4 @@
+from src.util.create_jwt_token import create_access_token
+
+if __name__ == "__main__":
+    print(create_access_token(customer_id="06c2acac-7810-4dd8-8722-54bdb05fb9e4"))
